a45c23dfafd9893dab08ee36cd09425e928d270c
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-04-06 22:34:05 -0500

    Prácticas y ejemplos

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,9 @@
+/*
+  node main.js
+*/
+
+const info = require('./info');
+const logger2 = require('./verbose');
+
+info('This is an informational message');
+logger2.verbose('This is a verbose message');
