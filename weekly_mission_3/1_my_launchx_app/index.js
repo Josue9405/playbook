d46068536b7f6a6ccbbeb5886b2f792c7c972188
Josue Efrain Avelar Dominguez <joshy_je@hotmail.com>
2022-04-20 22:01:16 -0500

    Eliminación de prácticas para su segmentación

--- weekly_mission_3/1_my_launchx_app/index.js
+++
@@ -1,7 +0,0 @@
-const MissionCommander = require('./app/MissionCommander')
-
-const mcOnboarding = new MissionCommander('Fer')
-const mcFrontEnd = new MissionCommander('Rodrigo')
-const mcBackEnd = new MissionCommander('Carlo')
-
-console.log('Los commander \n'+mcOnboarding.name+'\n'+mcFrontEnd.name+'\n'+mcBackEnd.name)
--- weekly_mission_3/1_my_launchx_app/test/MissionCommander.test.js
+++
@@ -1,8 +0,0 @@
-const MissionCommander = require('./../app/MissionCommander');
-
-describe("Unit Tests for Mission Commander Class", () => {
-    test('1) Create a mission commander objet', () => {
-        const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Woopa");
-    });
-  })  
